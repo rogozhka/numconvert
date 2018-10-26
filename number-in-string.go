44c86b5a6af5e27ad3259b1a455a642dfc39250d
rogozhka <serge.rogozhkin@gmail.com>
2018-10-26 19:42:24 +0300

    Generalized errors when overflow & convert fails

--- number-in-string.go
+++ number-in-string.go
@@ -35,7 +35,7 @@ func (s NumberInString) AsUint64() (uint64, error) {
 
 	bi := big.NewInt(0)
 	if _, ok := bi.SetString(string(s), 10); !ok {
-		return 0, fmt.Errorf("Cannot convert to uint64 from: %v", s)
+		return 0, convertErrorf(s, "uint64", nil)
 	}
 
 	return bi.Uint64(), nil
@@ -45,7 +45,7 @@ func (s NumberInString) AsInt64() (int64, error) {
 
 	bi := big.NewInt(0)
 	if _, ok := bi.SetString(string(s), 10); !ok {
-		return 0, fmt.Errorf("Cannot convert to int64 from: %v", s)
+		return 0, convertErrorf(s, "int64", nil)
 	}
 
 	return bi.Int64(), nil
@@ -59,10 +59,10 @@ func (s NumberInString) AsInt32() (int, error) {
 
 	res, err := strconv.Atoi(string(s))
 	if err != nil {
-		return 0, fmt.Errorf("Cannot convert to int from: %v | %v", s, err)
+		return 0, convertErrorf(s, "int", err)
 	}
 	if res > MaxInt32 || res < -MaxInt32 {
-		return 0, fmt.Errorf("Overflow maxInt32:%v", s)
+		return 0, overflowErrorf(s, "MaxInt32")
 	}
 
 	return res, nil
@@ -75,10 +75,10 @@ func (s NumberInString) AsUint32() (uint, error) {
 
 	res, err := strconv.Atoi(string(s))
 	if err != nil {
-		return 0, fmt.Errorf("Cannot convert to uint from: %v | %v", s, err)
+		return 0, convertErrorf(s, "uint32", nil)
 	}
 	if res > MaxUint32 || res < 0 {
-		return 0, fmt.Errorf("Overflow maxUnt32:%v", s)
+		return 0, overflowErrorf(s, "MaxUint32")
 	}
 
 	return uint(res), nil
@@ -88,10 +88,10 @@ func (s NumberInString) AsUint16() (uint, error) {
 
 	res, err := strconv.Atoi(string(s))
 	if err != nil {
-		return 0, fmt.Errorf("Cannot convert to uint from: %v | %v", s, err)
+		return 0, convertErrorf(s, "uint16", nil)
 	}
 	if res > MaxUint16 || res < 0 {
-		return 0, fmt.Errorf("Overflow maxUint16:%v", s)
+		return 0, overflowErrorf(s, "MaxUint16")
 	}
 
 	return uint(res), nil
@@ -101,10 +101,10 @@ func (s NumberInString) AsUint8() (uint, error) {
 
 	res, err := strconv.Atoi(string(s))
 	if err != nil {
-		return 0, fmt.Errorf("Cannot convert to uint from: %v | %v", s, err)
+		return 0, convertErrorf(s, "uint8", nil)
 	}
 	if res > MaxUint8 || res < 0 {
-		return 0, fmt.Errorf("Overflow maxUint8:%v", s)
+		return 0, overflowErrorf(s, "MaxUint8")
 	}
 
 	return uint(res), nil
@@ -114,10 +114,10 @@ func (s NumberInString) AsInt16() (int, error) {
 
 	res, err := strconv.Atoi(string(s))
 	if err != nil {
-		return 0, fmt.Errorf("Cannot convert to int from: %v | %v", s, err)
+		return 0, convertErrorf(s, "int16", nil)
 	}
 	if res > MaxInt16 || res < -MaxInt16 {
-		return 0, fmt.Errorf("Overflow maxInt16:%v", s)
+		return 0, overflowErrorf(s, "MaxInt16")
 	}
 
 	return res, nil
@@ -127,11 +127,25 @@ func (s NumberInString) AsInt8() (int, error) {
 
 	res, err := strconv.Atoi(string(s))
 	if err != nil {
-		return 0, fmt.Errorf("Cannot convert to int from: %v | %v", s, err)
+		return 0, convertErrorf(s, "int8", nil)
 	}
 	if res > MaxInt8 || res < -MaxInt8 {
-		return 0, fmt.Errorf("Overflow maxInt8:%v", s)
+		return 0, overflowErrorf(s, "MaxInt8")
 	}
 
 	return res, nil
+}
+
+func convertErrorf(from interface{}, to string, err error) error {
+
+	var strErr string
+	if err != nil {
+		strErr = err.Error()
+	}
+
+	return fmt.Errorf("Cannot convert to | %s | %s | %s", to, from, strErr)
+}
+
+func overflowErrorf(from interface{}, subj string) error {
+	return fmt.Errorf("Overflow | %s | %s", subj, from)
 }
